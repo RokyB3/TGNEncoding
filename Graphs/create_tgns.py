b7079e31cb5923332a0d3f17b882453c898e8d9b
RokyB3 <roko.baljak@mail.mcgill.ca>
2025-03-26 18:30:56 -0400

    Added create tgn method

--- Graphs/create_tgns.py
+++ Graphs/create_tgns.py
@@ -5,13 +5,30 @@ from ba_graphs import create_BA_graph
 
 ba_graph = create_BA_graph(5, 2)
 
-print("ba_graph: ", ba_graph)
 step_config = StepConfig(n_add=2, p_add=0.5, p_remove=0.5)
 
-edges, new_nodes, removed_nodes, removed_edges, added_edges = step_graph(ba_graph, step_config)
+def create_tgn(ba_graph, step_config, iterations):
+    tgn = [{
+        "edges": ba_graph,
+        "nodes": set(ba_graph.flatten()),
+        "new_nodes": set(),
+        "removed_nodes": set(),
+        "removed_edges": set(),
+        "added_edges": set()
+    }]
 
-print("edges: ", edges)
-print("new_nodes: ", new_nodes)
-print("removed_nodes: ", removed_nodes)
-print("removed_edges: ", removed_edges)
-print("added_edges: ", added_edges)
+    for i in range(iterations-1):
+        edges, new_nodes, removed_nodes, removed_edges, added_edges = step_graph(ba_graph, step_config)
+        tgn.append({
+            "edges": edges,
+            "nodes": set(edges.flatten()),
+            "new_nodes": new_nodes,
+            "removed_nodes": removed_nodes,
+            "removed_edges": removed_edges,
+            "added_edges": added_edges
+        })
+        ba_graph = edges
+    return tgn
+
+
+print("tgn: ", create_tgn(ba_graph, step_config, 3))
